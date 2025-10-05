c978b2ec052c014c3ee0fb875c33e5f04bda3cc6
Hasnain-Ahmad48 <hasnaink1480@gmail.com>
2025-10-05 15:55:27 -0400

    getting started widh function

---
+++ calculateArea/calculate_Area.js
@@ -0,0 +1,11 @@
+let lenght;
+let width;
+
+function calculateArea(){
+    lenght= parseFloat(document.getElementById('length').value)
+    width=parseFloat(document.getElementById('width').value)
+
+    let area = lenght * width
+
+    document.getElementById('result').innerHTML=`The area the recrangle is: ${area}`
+}
