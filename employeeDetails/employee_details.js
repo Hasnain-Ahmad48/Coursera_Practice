1aba2544b6d59d5fd18048b3eb10ba3ba0ebfbde
Hasnain-Ahmad48 <hasnaink1480@gmail.com>
2025-10-09 21:16:19 +0500

    task done of coursra

--- employeeDetails/employee_details.js
+++ employeeDetails/employee_details.js
@@ -5,6 +5,7 @@ const employees = [
     age: 30,
     department: "IT",
     salary: 50000,
+    // spacialization: "javascript",
   },
   {
     id: 2,
@@ -12,6 +13,7 @@ const employees = [
     age: 28,
     department: "HRE",
     salary: 45000,
+    spacialization: "Python",
   },
   {
     id: 3,
@@ -19,15 +21,19 @@ const employees = [
     age: 35,
     department: "Finance",
     salary: 60000,
+    spacialization: "Java",
   },
 ];
 
 // Function to display all employees
 function displayEmployees() {
-    const totalEmployees = employees
-        .map(employee => `<p>${employee.id}: ${employee.name} - ${employee.department} - $${employee.salary}</p>`)
-        .join('');
-    document.getElementById('employeesDetails').innerHTML = totalEmployees;
+  const totalEmployees = employees
+    .map(
+      employee =>
+        `<p>${employee.id}: ${employee.name} - ${employee.department} - $${employee.salary}</p>`
+    )
+    .join("");
+  document.getElementById("employeesDetails").innerHTML = totalEmployees;
 }
 
 function calculateTotalSalaries() {
@@ -60,5 +66,20 @@ function findEmployeeById(employeeId) {
   } else {
     document.getElementById("employeesDetails").innerHTML =
       "no employee has been found with this ID";
+  }
+}
+
+function findEmployeeBySpelizayion() {
+  const foundEmployee = employees.find(
+    employee => employee.spacialization === "javascript"
+  );
+  console.log(foundEmployee);
+  if (foundEmployee) {
+    document.getElementById(
+      "employeesDetails"
+    ).innerHTML = `<p>${foundEmployee.id}: ${foundEmployee.name} - ${foundEmployee.department} - ${foundEmployee.spacialization}</p>`;
+  }
+  else{
+    document.getElementById("employeesDetails").innerHTML=`<p>User not found</p>`
   }
 }
