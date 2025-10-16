de7893eee45db192a6069268577d531de9040be7
Hasnain-Ahmad48 <hasnaink1480@gmail.com>
2025-10-16 23:39:04 +0500

    wether app

--- weatherReport/weather_report.js
+++ weatherReport/weather_report.js
@@ -1,3 +1,17 @@
 function showweatherDetails(event) {
-      event.preventDefault();
+  event.preventDefault();
+  const city = document.getElementById("city").value;
+  const apiKey = "d266d7dbbb460a92e78e4a4afc4d688e"; // Replace 'YOUR_API_KEY' with your actual API key
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+      const weatherInfo = document.getElementById("weatherInfo");
+      weatherInfo.innerHTML = `<h2>Weather in ${data.name}</h2>
+                                    <p>Temperature: ${data.main.temp} &#8451;</p>
+                                    <p>Weather: ${data.weather[0].description}</p>`;
+    });
 }
+
+ document.getElementById('weatherForm').addEventListener('submit',showweatherDetails );
+
